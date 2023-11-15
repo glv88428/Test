243ba09a0889212006cbbc2a9061f57730bbaf44
glv88428 <glv88428.kwansei.ac.jp>
2023-11-15 18:42:35 +0900

    ex3

--- ex3.py
+++ ex3.py
@@ -1,1 +1,13 @@
-print("HELLO")
+import zipfile
+
+zip_f = zipfile.ZipFile('sample.zip')
+sum = 0
+
+
+for i in range(1,1000,2):
+    filename=f'sample/kitamura_{i:05}_kgu.txt'
+    #print(filename)
+    with zip_f. open(filename, 'r') as file:
+       number = int(file.read().strip())
+       sum+=number
+print('sum',sum)
