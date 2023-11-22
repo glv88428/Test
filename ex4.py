69b7b8333c0e205d078b5a6a9f986943b8ee6b78
glv88428 <glv88428.kwansei.ac.jp>
2023-11-22 17:47:38 +0900

    s

--- ex4.py
+++ ex4.py
@@ -1,5 +1,4 @@
 import tkinter as tk
-
 def draw(canvas):
     canvas.create_oval(150,50, 200,100)
     canvas.create_line(175,100, 175,160)
@@ -7,7 +6,6 @@ def draw(canvas):
     canvas.create_line(175,125, 215,100)
     canvas.create_line(175,160, 135,210)
     canvas.create_line(175,160, 215,210)
-
 root=tk.Tk()
 canvas=tk.Canvas(root,width=400,height=300)
 canvas.pack()
