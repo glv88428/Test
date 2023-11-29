3e0b74eceda6a3f1038cbb2f4c23933dc22e41a7
glv88428 <glv88428.kwansei.ac.jp>
2023-11-29 18:27:42 +0900

    6_2

--- ex6_2.py
+++ ex6_2.py
@@ -1,7 +1,19 @@
 import json
-import cv2
-import numpy as np
+import tkinter as tk
+from tkinter import Canvas
 import zipfile
+
+def read_keypoints_from_json(json_file):
+    with open(json_file, 'r') as f:
+        data = json.load(f)
+    return data['people']
+
+def draw_shoulder_line(canvas, shoulder):
+    canvas.create_line(shoulder[0], shoulder[1], shoulder[2], shoulder[3], fill="red", width=2)
+    
+def draw_shoulder_line2(canvas, shoulder):
+    canvas.create_line(shoulder[0], shoulder[1], shoulder[2], shoulder[3], fill="green", width=2)    
+    
 
 zip_f = zipfile.ZipFile('kabeposter.zip')
 json_file_path = "kabeposter/kabeposter_000000000000_keypoints.json"
@@ -9,26 +21,41 @@ json_file_path = "kabeposter/kabeposter_000000000000_keypoints.json"
 with zip_f.open(json_file_path, "r") as file:
     data = json.load(file)
 
-
 people_data = data["people"]
 
-shoulder1 = (
-    (int(people_data[0]["pose_keypoints_2d"][2]), int(people_data[0]["pose_keypoints_2d"][3])),
-    (int(people_data[0]["pose_keypoints_2d"][5]), int(people_data[0]["pose_keypoints_2d"][6]))
-)
 
-shoulder2 = (
-    (int(people_data[1]["pose_keypoints_2d"][2]), int(people_data[1]["pose_keypoints_2d"][3])),
-    (int(people_data[1]["pose_keypoints_2d"][5]), int(people_data[1]["pose_keypoints_2d"][6]))
-)
-width = 2000
-height = 2000
 
-image = np.zeros((height, width, 3), dtype=np.uint8)
+shoulder1 = [
+    int(people_data[0]["pose_keypoints_2d"][6]), int(people_data[0]["pose_keypoints_2d"][7]),
+    int(people_data[0]["pose_keypoints_2d"][3]), int(people_data[0]["pose_keypoints_2d"][4])
+]
 
-cv2.line(image, shoulder1[0], shoulder1[1], (0, 255, 0), 2)
-cv2.line(image, shoulder2[0], shoulder2[1], (0, 255, 0), 2)
+shoulder2 = [
+    int(people_data[0]["pose_keypoints_2d"][3]), int(people_data[0]["pose_keypoints_2d"][4]),
+    int(people_data[0]["pose_keypoints_2d"][15]), int(people_data[0]["pose_keypoints_2d"][16])
+]
+shoulder3 = [
+    int(people_data[1]["pose_keypoints_2d"][6]), int(people_data[1]["pose_keypoints_2d"][7]),
+    int(people_data[1]["pose_keypoints_2d"][3]), int(people_data[1]["pose_keypoints_2d"][4])
+]
 
-cv2.imshow("Shoulder Lines", image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+shoulder4 = [
+    int(people_data[1]["pose_keypoints_2d"][3]), int(people_data[1]["pose_keypoints_2d"][4]),
+    int(people_data[1]["pose_keypoints_2d"][15]), int(people_data[1]["pose_keypoints_2d"][16])
+]
+
+
+
+root = tk.Tk()
+root.title("Shoulder Lines")
+
+canvas = Canvas(root, width=2000, height=1000)
+canvas.pack()
+
+draw_shoulder_line(canvas, shoulder1)
+draw_shoulder_line2(canvas, shoulder2)
+draw_shoulder_line(canvas, shoulder3)
+draw_shoulder_line2(canvas, shoulder4)
+
+# イベントループの開始
+root.mainloop()
